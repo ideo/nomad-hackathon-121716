874b66048fc99654ae34b0165ce0923ddc5c3d35
Gavin McDermott <gavinmcdermott@gmail.com>
2016-12-06 16:14:08 -0800

    add README for hackathon

---
+++ README.md
@@ -0,0 +1,23 @@
+# IDEO Nomad Hackathon - 12.17.16
+
+## Pre-hackathon Setup
+
+Before the hackathon begins, you'll want to take the following steps:
+
+### Install IPFS 
+
+You will need IPFS to use Nomad. You can install the most up to date IPFS build [here](https://ipfs.io/).
+It is a wonderful project and team, so we encourage you to learn about it as well!
+
+### Install Node.js
+
+Nomad is currently only written in Javascript and the hackathon will be Node.js-based. 
+You can install the latest stable NodeJS build [here](https://nodejs.org/en/).
+
+### Clone This Repo
+
+To familiarize you with Nomad, we'll do a live walkthrough and build a 
+new Nomad node together at the beginning of the hackathon. 
+
+The code we'll build upon is in this repo, so feel free to clone it locally and 
+follow along with us in the live-demo introduction.
--- sensor.js
+++ sensor.js
@@ -1,28 +1,24 @@
 const Nomad = require('nomad-stream')
 const moment = require('moment')
+
 const nomad = new Nomad()
+const publishFrequency = 5 * 60 * 1000 // every 5 minutes
 
 let instance = null
-const birthdate = moment('2016-11-10')
 
-const publishFrequency = 5 * 60 * 1000
+const getLatestMessage = () => {
+  const now = moment().format('MMMM Do YYYY, h:mm:ss a').toString()
+	return `Hello from Nomad on ${now}`
+}
 
-function getMessage() {
-	const message = `Hello from the Nomad beacon!\nNomad was born ${birthdate.fromNow()} on ${birthdate.format('MMMM Do, YYYY')}.\nThis message was sent at ${moment().format('h:mm a')} UTC.\nIt's a plain text message, but nodes can publish structured data too as stringified JSON.`
-
-		return message
-	}
-
-nomad.prepareToPublish().then((n) => {
-  instance = n
-  return instance.publishRoot(getMessage())
-}).then(() => {
-  setInterval(() => {
-    instance.publish(getMessage())
-    .catch(err => {
-      console.log(`Error: ${err}`)
-    })
-  }, publishFrequency)  
-})
-
-// console.log(getMessage())
+nomad.prepareToPublish()
+  .then((node) => {
+    instance = node
+    return instance.publishRoot(getLatestMessage())
+  }).then(() => {
+    setInterval(() => {
+      instance
+        .publish(getLatestMessage())
+        .catch(err => console.log(`Error: ${err}`))
+    }, publishFrequency)
+  })
